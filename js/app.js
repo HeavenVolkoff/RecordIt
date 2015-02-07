2b3799cebfc743dca592d2b19748d178d178f49a
Vítor Augusto da Silva Vasconcellos <vasvas10@gmail.com>
2015-02-07 04:20:39 -0200

    Removed RecordRTC, added MediaStreamRecorder, started simultaneous play and record logic

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@
 		/**
 		 * DOMContentLoaded Listener
 		 */
+
+		reader: new window.FileReader(),
+
 		domLoaded: function() {
 			/*SideBar init*/
 			$(".button-collapse").sideNav();
@@ -26,29 +29,97 @@
 
 		startRecording: function(stream){
 			/* Recorder Init */
-			app.Microfone.recorder = RecordRTC(stream);
-			app.Microfone.recorder.startRecording();
+			app.Microfone.recorder = new MediaStreamRecorder(stream);
+			app.Microfone.recorder.mimeType = app.audioType;
+			app.Microfone.recorder.ondataavailable = this.audioData;
+			app.Microfone.recorder.start(160000);
+
 			$('#micBtn').find('i').removeClass("mdi-av-mic").addClass('mdi-av-mic-off');
 			$('.record-text').text('Recording...');
 		},
 
-		stopRecording: function(){
-			app.Microfone.recorder.stopRecording(function(audioURL){
-				$('#micBtn').find('i').removeClass('mdi-av-mic-off').addClass("mdi-av-mic");
-				$('.record-text').text('Record Stoped');
+		audioData: function(blob){
+			var noRecords = $('.noRecords');
 
-				var changeRecordText = setTimeout(function(){
-					$('.record-text').text('Start Record');
-				}, 1000);
+			async.waterfall(
+				[
+					function(callback){
+						if(typeof app.Microfone.currentRecord === 'undefined'){
+							var fileRequest = app.sdCard.getEditable(app.Microfone.currentRecordName);
 
+							fileRequest.onsuccess = function(fileHandler){
+								app.Microfone.currentRecord = fileHandler.open('readwrite');
+								callback(null, app.Microfone.currentRecord);
+							};
 
-				$('.noRecords').remove();
-				$('.records').append($('<li>').addClass('collection-item').append($('<audio>').attr('src', audioURL).prop('controls', true)));
-			});
+							fileRequest.onerror = function(){
+								app.sdCard.addNamed(blob, app.folderName + app.Microfone.currentRecordName);
+								callback(null, null);
+							};
+						}else{
+							callback(null, app.Microfone.currentRecord);
+						}
+					},
+					function(record, callback){
+						if(record){
+							if(record.active){
+								var writeHandler = app.Microfone.currentRecord.append(blob);
+
+								writeHandler.onProgress(function(status){
+									console.log('Appending audio chunk to file.\n Chunk size: ' + status.total + ' bytes, already saved: ' + status.loaded + ' bytes');
+								});
+
+								writeHandler.onsuccess(function(){
+									callback();
+								});
+
+								writeHandler.onerror(function(){
+									callback(this.error);
+								});
+							}
+						}else{
+							callback();
+						}
+					}
+				]
+			);
+
+			controller.reader.onloadend = function() {
+				var audioURL = controller.reader.result;
+
+				console.log(audioURL);
+
+				if(){
+
+				}else{
+					ConcatenateBlobs([currentRecord.attr('src'), blob], app.audioType, function(concatenated) {
+						currentRecord.attr('src', concatenated);
+					});
+				}
+			};
+			controller.reader.readAsDataURL(blob);
+		},
+
+		stopRecording: function(error){
+			app.Microfone.recorder.stop();
+			$('#micBtn').find('i').removeClass('mdi-av-mic-off').addClass("mdi-av-mic");
+			$('.record-text').text('Record Stopped');
+
+			var changeRecordText = setTimeout(function(){
+				$('.record-text').text('Start Record');
+			}, 1000);
 		}
 	};
 
 	var app = {
+		sdCard: navigator.getDeviceStorage("sdcard"),
+
+		folderName: 'records/',
+
+		audioType: 'audio/ogg',
+
+		today: new Date(),
+
 		/**
 		 * Initialize App Base Function
 		 */
@@ -56,8 +127,31 @@
 			this.Microfone = {
 				waveSurfer: Object.create(WaveSurfer),
 				waveMic: Object.create(WaveSurfer.Microphone),
-				recorder: undefined
+				recorder: undefined,
+				currentRecord: null,
 			};
+
+			Object.defineProperties(this.Microfone, {
+				currentRecordName: {
+					get: function () {
+						return 'record-' + app.today.getDate() + '-' +
+							app.today.getMonth() + 1 + '-' +
+							app.today.getYear() + '-' +
+							app.today.getHours() + '-' +
+							app.today.getMinutes() + '-' +
+							app.today.getSeconds() + '.ogg';
+					}
+				},
+				recording: {
+					get: function(){
+						if(app.Microfone.recorder){
+
+						}else{
+							return false;
+						}
+					}
+				}
+			});
 
 			this.bindEvents();
 		},
